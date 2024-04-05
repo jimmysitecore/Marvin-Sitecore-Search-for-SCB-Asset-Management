ff2de994985e2bb57f6633db3a234cc0ca8cdca0
jimmysitecore <jimmy.wong@sitecore.com>
2024-04-05 14:40:08 +0800

    update logo
    update logo

--- src/components/Icons/Logo.jsx
+++ src/components/Icons/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
   // <img src="https://www.scbam.com/medias/upload/home/scbam-logo-en.png" width="100%" alt="SCB Logo" />
-  <img src="https://www.moe.gov.sg/-/media/moe/assets/moe-logo.svg" width="100%" alt="MOE Logo" />
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HKEX_logo_2016.svg/2560px-HKEX_logo_2016.svg.png" width="100%" alt="MOE Logo" />
 
   );
 };
